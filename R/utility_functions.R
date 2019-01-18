b4a8a43d4ad02fb41578b4992556638f80e44cb2
Sebastian Bayer <bayerse@gmail.com>
2019-01-18 21:05:23 +0100

    update risk forecasts data

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -6,7 +6,8 @@
 #' @param q A vector of expected shortfall forecasts
 #' @param e A vector of Value-at-Risk forecasts
 #' @param s A vector of volatility forecasts
+#' @param H A vector of forecasts of cumulative violations
 #' @param alpha Scalar probability level in (0, 1)
 #' @param B Number of bootstrap samples. Set to 0 to disable bootstrapping.
 #' @keywords internal
-parameter_definition <- function(r, q, e, s, alpha) {}
+parameter_definition <- function(r, q, e, s, H, alpha) {}
